3af572b376af8d47be58971a6b1fa175fab4f97c
kasshii28 <gp22a043@oecu.jp>
2024-12-24 11:11:27 +0900

    add DI

---
+++ DI/objectInitializeDI.go
@@ -0,0 +1,29 @@
+package DI
+
+type ServiceImpl struct {}
+
+func (s *ServiceImpl) Apply(id int) error {
+	return nil
+}
+
+type OrderService interface {
+	Apply(int) error
+}
+
+type Application struct {
+	os OrderService
+}
+
+func NewApplication(os OrderService) *Application {
+	return &Application{os: os}
+}
+
+func (app *Application) Apply(id int) error {
+	return app.os.Apply(id)
+}
+
+func main() {
+	app := NewApplication(&ServiceImpl{})
+	app.Apply(19)
+}
+
